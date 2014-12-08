80481d3b3c0a0f33baa8c17d23451316bcb661f9
vivekmshah <vivek.shah@falkonry.com>
2014-12-07 20:22:53 -0800

    add notes create controller and api endpoint

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,4 +10,26 @@ class NotesController < ApplicationController
 
   end
 
+  def create
+
+    # MISSING USER_ID WHEN USING API ENDPOINT -> 
+    note = Note.new(note_params)
+
+    respond_to do |format|
+      if note.save
+        format.html {  }
+        format.json { render json: note, status: :created }
+      else
+        format.html {  }
+        format.json { render json: note.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def note_params
+    params.require(:note).permit(:description)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   scope 'api', defaults: {format: :json} do
-    resources :notes, :only => [:index]
+    resources :notes, :only => [:index, :create]
   end
 
 end
