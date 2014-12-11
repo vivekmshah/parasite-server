dc8483b5318fa211cbadf165b3125ec4f175d6be
vivekmshah <vivek.shah@falkonry.com>
2014-12-10 21:29:30 -0800

    separate wndx DOM and JS resources

---
+++ public/wndx.js
@@ -0,0 +1,25 @@
+// var domainName = document.domain;
+// var domain = domainName.replace('www.','');
+
+httpRequest = new XMLHttpRequest();
+// httpRequest.open('GET', '//fast-ocean-4567.herokuapp.com/api/notes?domain=' + domain, true);
+httpRequest.open('GET', '//localhost:3000/api/notes', true);
+console.log("Hmmm...what's going on.");
+
+httpRequest.onreadystatechange = function(){
+  if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+    console.log("AJAX request successfully made.");
+    var myArr = JSON.parse(httpRequest.responseText);
+    document.getElementById('postCC').innerHTML = stylize(myArr);
+  }
+};
+
+httpRequest.send();
+
+function stylize(arr) {
+  var out = "";
+  for(var i = 0; i < arr.length; i++) {
+    out += '<div class="postContainer"> <p class="Post"> <span class="userName">@' + 'USERNAME' + ':</span><span class="postBody">' + arr[i].description + '</span></p><p class="commentsLink">' + 'COMMENT COUNT' + '</p></div>';
+  }
+  return out;
+}
