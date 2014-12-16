b1aac587746fb92d43fab38383c137c38524c824
vivekmshah <vivek.shah@falkonry.com>
2014-12-16 13:54:48 -0800

    single rails view template - eli ux

--- public/wndx.js
+++ public/wndx.js
@@ -1,34 +1,97 @@
-var domainName = document.domain;
-var domain = domainName.replace('www.','');
+$( document ).ready(function() {
 
-httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', '//fast-ocean-4567.herokuapp.com/api/notes?domain=' + domain, true);
-// httpRequest.open('GET', 'http://localhost:3000/api/notes', true);
-console.log("Hmmm...what's going on.");
+  $('#add-post-container').toggle();
 
-httpRequest.onreadystatechange = function(){
-  if (httpRequest.readyState == 4 && httpRequest.status == 200) {
-    console.log("AJAX request successfully made.");
-    var myArr = JSON.parse(httpRequest.responseText);
-    document.getElementById('postCC').innerHTML = stylize(myArr);
-  }
-};
+  $('#hide-button').click(function() {
+    $('#wndx-sidebar').css("right", "-400px");
+    $('#show-button').css("right", "-20px");
+    $('.commentCC').css("right", "-380px");
+    $('.userCC').css("right", "-380px");
+    $('#sessionCC').css("right", "-380px");
+  });
 
-httpRequest.send();
+  $('#show-button').click(function() {
+    $('#wndx-sidebar').css("right", "0px");
+    $('#show-button').css("right", "-100px");
+  });
 
-// TEMPLATE
-// <div class="postContainer">
-//   <p class="Post">
-//     <span class="userName">@alexhart:</span> 
-//     <span class="postBody">This is a four line post that I’m writing to check on this fonts legibility for body copy.</span>
-//   </p>
-//   <p class="commentsLink">44 Comments</p>
-// </div>
+  $('#addPost').click(function() {
+    $('#postCC').css("margin-top", "280px");
+    $('#addPost').css("opacity", "0");
+    $('#addPost').toggle();
+    $('#add-post-container').toggle();
+    $('#add-post-input').css("opacity", "1");
+      $('#add-post-input').css("height", "80px");
+  });
 
-function stylize(arr) {
-  var out = "";
-  for(var i = 0; i < arr.length; i++) {
-    out += '<div class="postContainer"> <p class="Post"> <span class="userName">@' + 'USERNAME' + ':</span><span class="postBody">' + arr[i].description + '</span></p><p class="commentsLink">' + 'COMMENT COUNT' + '</p></div>';
-  }
-  return out;
-}
+  $('#submitPost').click(function() {
+    $('#postCC').css("margin-top", "90px");
+    $('#addPost').css("opacity", "1");
+    $('#add-post-container').toggle();
+    $('#add-post-input').css("opacity", "0");
+    $('#add-post-input').css("height", "0px");
+  });
+
+  $('#closePost').click(function() {
+    $('#postCC').css("margin-top", "90px");
+    $('#addPost').css("opacity", "1");
+    $('#addPost').toggle();
+    $('#add-post-container').toggle();
+    $('#add-post-input').css("opacity", "0");
+    $('#add-post-input').css("height", "0px");
+  });
+
+  $('.commentsLink').click(function() {
+    $('.commentCC').css("right", "-10px");
+    $('.hide-comments-vert-button').css("right", "370px");
+  });
+
+  $('#hide-comments-button').click(function() {
+    $('.commentCC').css("right", "-380px");
+    $('.hide-comments-vert-button').css("right", "0px");
+  });
+
+  $('.hide-comments-vert-button').click(function() {
+    $('.commentCC').css("right", "-380px");
+    $('.hide-comments-vert-button').css("right", "0px");
+  });
+
+  $('.hide-comments-vert-button').mouseover(function() {
+    $('.commentCC').css("border-left", "1px solid rgba(255,255,255,.8)");
+  });
+
+  $('.hide-comments-vert-button').mouseout(function() {
+    $('.commentCC').css("border-left", "1px solid rgba(255,255,255,.1)");
+
+  });
+
+  $('.userName').click(function() {
+    $('.userCC').css("right", "-10px");
+  });
+
+  $('#hide-user-button').click(function() {
+    $('.userCC').css("right", "-380px");
+    $('.hide-user-vert-button').css("right", "0px");
+  });
+
+  $('#login').click(function() {
+    $('#sessionCC').css("right", "-10px");
+      $('#login').toggle();
+    $('#signup').toggle();
+
+  });
+
+  $('#signup').click(function() {
+    $('#sessionCC').css("right", "-10px");
+      $('#login').toggle();
+    $('#signup').toggle();
+  });
+
+  $('#hide-session-button').click(function() {
+    $('#sessionCC').css("right", "-380px");
+    $('#login').toggle();
+    $('#signup').toggle();
+
+  });
+
+});
