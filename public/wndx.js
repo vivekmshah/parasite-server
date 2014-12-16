bde343cf448581b5891552a66522f73bf2866b18
vivekmshah <vivek.shah@falkonry.com>
2014-12-16 11:21:46 -0800

    first attempt

--- public/wndx.js
+++ public/wndx.js
@@ -32,3 +32,26 @@ function stylize(arr) {
   }
   return out;
 }
+
+
+
+
+$('wndx-sidebar input#login').click(function() {
+  console.log('hello?');
+  var AUTH_TOKEN = $('meta[name="csrf-token"]').attr('content');
+  $.ajaxSetup({
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('X-CSRF-Token', AUTH_TOKEN);
+      }
+    }); 
+
+  $.ajax({
+      type: "POST", 
+      url: "/login?&authenticity_token="+encodeURIComponent(AUTH_TOKEN), 
+      data:{ username : $('#username').val() , password : $('#password').val() },
+      dataType: "JSON"
+      //success:window.location.replace("/")
+      });
+
+  return false;
+});
