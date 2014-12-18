9f405629410c73b00334f1b8b6a9a67c7c00025d
elikagan <eli.kagan@gmail.com>
2014-12-17 23:41:14 -0800

    remove commented session create code reference in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,27 +32,6 @@ class UsersController < ApplicationController
 
   end
 
-# CREATE SESSION CODE
-  # def create
-  #     user = User.where(username: params[:username]).first
-  #     # first make sure we actually find a user
-  #     # then see if user authenticates
-  #     if user && user.authenticate(params[:password])
-  #         # sets the cookie to the browser
-          # session[:user_id] = user.id
-          # render json: {login: true, user_id: user.id}
-  #     else
-  #         # flash.now.alert = "Email or password is invalid"
-  #         render json: {login: false}
-  #         render "new"
-  #         # redirect_to new_session_path
-  #     end
-  # end
-
-
-
-
-
   def update
     @user.update_attributes(user_params)
   end
